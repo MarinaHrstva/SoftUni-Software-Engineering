ad3d55a871d2ead0b4b72a2cb5a9e8081b358381
Marina Hristova <marina.ts.hristova@gmail.com>
2022-01-12 10:58:56 +0200

    Add file

---
+++ JS-Fundamentals/06-objects-and-classes/inventory.js
@@ -0,0 +1,30 @@
+function inventory(input) {
+
+    input.map(line => {
+        let [name, level, items] = line.split(' / ')
+              items = items.split(', ')
+            .sort((a, b) => a.localeCompare(b))
+            .join(', ')
+        return {
+            name, 
+            level: Number(level),
+            items  
+        }
+    }).sort((a, b) => a.level - b.level)
+        .forEach(hero => {
+            console.log(`Hero: ${hero.name}`);
+            console.log(`level => ${hero.level}`);
+            console.log(`items => ${hero.items}`);
+        });
+
+
+
+}
+
+
+inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+]
+)
