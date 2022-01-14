da7a1eb3e8e54eb6ca5f42df68bace6fd740d1ca
Marina Hristova <marina.ts.hristova@gmail.com>
2022-01-14 15:10:58 +0200

    Delete files

--- JS-Advanced/04-DOM-intro/01.Edit Element/EditElement.js
+++
@@ -1,7 +0,0 @@
-function editElement(element,matcher,replacer) {
-    let text=element.textContent
-    let pattern= new RegExp(matcher,'g')
-    let editted=text.replace(pattern,replacer)
-    element.textContent=editted
-
-}
