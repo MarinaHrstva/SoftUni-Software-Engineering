fecaffaa3148c8ae8e55571918908dd16b83c036
Marina Hristova <marina.ts.hristova@gmail.com>
2022-01-14 15:09:13 +0200

    Add file

---
+++ JS-Fundamentals/08-text-processing/pascla-case-spliter.js
@@ -0,0 +1,16 @@
+function pascalCaseSpliter(str) {
+    let result = [];
+    let upperIndex = 0;
+    for (let i = 1; i < str.length; i++) {
+        if (str[i] === str[i].toUpperCase()) {
+            let word = str.substring(upperIndex, i);
+            result.push(word);
+            upperIndex = i;
+        }
+    }
+    result.push(str.substring(upperIndex))
+    console.log(result.join(', '));
+
+}
+
+pascalCaseSpliter('SplitMeIfYouCanHaHaYouCantOrYouCan')
