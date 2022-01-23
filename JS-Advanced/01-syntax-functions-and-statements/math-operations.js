d83201a59f09ace1b7a12047c84b709d4eac7a16
Marina Hristova <marina.ts.hristova@gmail.com>
2022-01-23 12:41:26 +0200

    Add file

---
+++ JS-Advanced/01-syntax-functions-and-statements/math-operations.js
@@ -0,0 +1,17 @@
+function mathOperations(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    case "%":
+      return a % b;
+    case "**":
+      return a ** b;
+  }
+}
+mathOperations(3, 5.5, "*");
