02ceb6b4004a791c07fb6431fe5905c5c8ce869f
Marina Hristova <marina.ts.hristova@gmail.com>
2022-03-10 17:27:03 +0200

    Add solutions

--- JS-Advanced/04-DOM-introduction/06.Sum Table/SumTable.js
+++ JS-Advanced/04-DOM-introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
+let prices=document.querySelectorAll('table tbody tr td:last-of-type');
 
+let sum=0;
+
+for (const element of prices) {
+    let price=Number(element.textContent);
+    if(price){
+        sum+=price
+    }
+}
+document.getElementById('sum').textContent=sum
 }
