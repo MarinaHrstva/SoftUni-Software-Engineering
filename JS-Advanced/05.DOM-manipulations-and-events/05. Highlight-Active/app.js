340d5885a15674bc8f13bf2a708bf9b65b71c238
Marina Hristova <marina.ts.hristova@gmail.com>
2022-03-17 22:54:44 +0200

    Add a solution

--- JS-Advanced/05.DOM-manipulations-and-events/05. Highlight-Active/app.js
+++ JS-Advanced/05.DOM-manipulations-and-events/05. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-    console.log('TODO:...');
+    let inputElements = document.querySelectorAll('input');
+
+    for (const el of inputElements) {
+        let parentElement = el.parentNode;
+
+        el.addEventListener('focus', () => {
+            parentElement.className = 'focused'
+        })
+
+        el.addEventListener('blur', () => {
+            parentElement.className = ''
+        })
+    }
 }
