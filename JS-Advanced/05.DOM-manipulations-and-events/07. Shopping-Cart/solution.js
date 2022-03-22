7c43db9ab589c59c7d44f6a650205a2e3490245d
Marina Hristova <marina.ts.hristova@gmail.com>
2022-03-22 16:10:00 +0200

    Add a solution

--- JS-Advanced/05.DOM-manipulations-and-events/07. Shopping-Cart/solution.js
+++ JS-Advanced/05.DOM-manipulations-and-events/07. Shopping-Cart/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-   //TODO...
+   document.getElementsByClassName('shopping-cart')[0].addEventListener('click', onClick);
+   document.getElementsByClassName('checkout')[0].addEventListener('click', checkout);
+
+   let output = document.getElementsByTagName('textarea')[0];
+   const cart = [];
+
+
+   function onClick(e) {
+      if (e.target.tagName == 'BUTTON' && e.target.classList.contains('add-product')) {
+         let product = e.target.parentNode.parentNode;
+         let name = product.querySelector('.product-title').textContent;
+         let price = Number(product.querySelector('.product-line-price').textContent);
+
+         cart.push({
+            name,
+            price
+         })
+         output.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`
+
+      }
+
+   }
+
+   function checkout() {
+      const products = new Set();
+      cart.forEach(p => products.add(p.name));
+
+      const total = cart.reduce((t, c) => t + c.price, 0)
+      output.value += `You bought ${[...products.keys()].join(', ')} for ${total.toFixed(2)}.`
+   }
+
 }
