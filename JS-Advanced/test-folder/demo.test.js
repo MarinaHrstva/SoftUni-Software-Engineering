dc60dea96a5ae9f6cdb2f6e375be446f984a1bc3
Marina Hristova <marina.ts.hristova@gmail.com>
2022-03-26 23:42:26 +0200

    Add a solution

--- JS-Advanced/test-folder/demo.test.js
+++ JS-Advanced/test-folder/demo.test.js
@@ -1,41 +1,14 @@
-const rgbToHexColor = require('./demo');
+const createCalculator = require('./demo');
 const { assert, expect} = require('chai');
 
-	// Take an array as argument
-	// Return false for any input that isn’t of the correct type
-	// Return true if the input array is symmetric
-	// Otherwise, return false
-
-
-describe('Test rgbToHexColor function', () => {
+	
+describe('Test createCalculator function', () => {
   
     it('test with white',()=>{
-        expect(rgbToHexColor(255,255,255)).to.equal('#FFFFFF');
+        expect(createCalculator(255,255,255)).to.equal('#FFFFFF');
     })
 
-    it('test with black',()=>{
-        expect(rgbToHexColor(0,0,0)).to.equal('#000000');
-    })
-
-    it('test with negative red number',()=>{
-        expect(rgbToHexColor(-1,255,255)).to.be.undefined;
-    })
-
-    it('test with negative green number',()=>{
-        expect(rgbToHexColor(255,-1,255)).to.be.undefined;
-    })
-
-    it('test with negative green number',()=>{
-        expect(rgbToHexColor(255,-1,255)).to.be.undefined;
-    })
-
-    it('test with missing number',()=>{
-        expect(rgbToHexColor(255,255)).to.be.undefined;
-    })
-
-    it('test with bigger number',()=>{
-        expect(rgbToHexColor(255,255,256)).to.be.undefined;
-    })
+    
     
     
 })
