d2f426a5aeb97a941ba3d54133a9d69074c24b82
Marina Hristova <marina.ts.hristova@gmail.com>
2022-05-15 17:25:16 +0300

    Add a solution to task 2

--- JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/lab-demo/02.Github-Repos/scripts/app.js
+++ JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/lab-demo/02.Github-Repos/scripts/app.js
@@ -1,3 +1,35 @@
 function loadRepos() {
-	console.log("TODO...");
+	let username = document.getElementById('username').value;
+	let url = `https://api.github.com/users/${username}/repos`;
+	let list = document.getElementById('repos')
+
+	fetch(url)
+		.then(res => {
+			if (res.ok == false) {
+				throw new Error(`${res.status} ${res.statusText}`)
+			}
+
+			return res.json();
+		})
+		.then(handleResponse)
+		.catch(handleError)
+
+
+	function handleResponse(data) {
+		list.innerHTML = '';
+		for (const repo of data) {
+			let liElement = document.createElement('li');
+			liElement.innerHTML = `  <a href="${repo.html_url}">
+					${repo.full_name}
+				</a>`
+
+			list.appendChild(liElement)
+		}
+
+	}
+
+	function handleError(error) {
+		list.innerHTML = '';
+		list.textContent = `${error.message}`;
+	}
 }
