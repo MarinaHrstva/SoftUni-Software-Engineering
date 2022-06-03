f506dab56cca9a075da419c307a2ee36b2c50058
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-03 15:32:55 +0300

    Task 6- add get posts logic

--- JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
+++ JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
@@ -1,5 +1,16 @@
 function attachEvents() {
-    console.log('TODO...');
+    getAllPosts()
+
 }
 
 attachEvents();
+
+
+async function getAllPosts() {
+    const url = `http://localhost:3030/jsonstore/blog/posts`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+}
+
