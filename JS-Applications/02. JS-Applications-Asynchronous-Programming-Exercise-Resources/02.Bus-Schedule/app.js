3cc30c708b36ca3dbb7615429305e1c17cae9277
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-03 15:10:11 +0300

    Add a solution to task 2

--- JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    let infoElement = document.querySelector('#info span');
+
+    let stop = {
+        next: 'depot'
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function depart() {
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`
+        departBtn.disabled = true;
+        const res = await fetch(url);
+        stop = await res.json();
+
+        infoElement.textContent = stop.name;
+        arriveBtn.disabled = false;
+    }
+
+    async function arrive() {
+
+        arriveBtn.disabled = true;
+        infoElement.textContent = `Arriving at ${stop.name}`
+        departBtn.disabled = false;
+
     }
 
     return {
