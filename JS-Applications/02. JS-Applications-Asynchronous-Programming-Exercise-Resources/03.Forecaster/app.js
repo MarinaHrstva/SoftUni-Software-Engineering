494f3ca9e93ccdefdc9c9e12cabc5b6c85b9c4bf
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-06 14:48:19 +0300

    Display current weather

--- JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
+++ JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
@@ -12,9 +12,47 @@ async function displayWeather() {
     let searchedLocation = document.getElementById('location').value;
     searchedLocation = await getLocation(searchedLocation);
     const currentWeather = await getCurrentCondition(searchedLocation.code);
-const upcomingWeather
+    const upcomingWeather = await getUpcommingCondition(searchedLocation.code);
+    const forecastElement = document.getElementById('forecast');
+    forecastElement.style.display = 'block';
 
-    console.log(currentWeather);
+    const symbols = {
+        Sunny: '\&#x2600',
+        'Partly sunny': '&#x26C5',
+        Overcast: '&#x2601',
+        Rain: '&#x2614',
+        Degrees: '&#176'
+
+    }
+    
+    const currentDivElement = document.getElementById('current');
+    const forcastDivElement = document.createElement('div');
+    forcastDivElement.classList.add('forcasts');
+    currentDivElement.appendChild(forcastDivElement);
+
+    const symbolSpanElement = document.createElement('span');
+    symbolSpanElement.classList.add('symbol', 'condition');
+    symbolSpanElement.innerHTML = `${symbols[currentWeather.forecast.condition]}`;
+    forcastDivElement.appendChild(symbolSpanElement);
+
+    const conditionElement = document.createElement('span');
+    conditionElement.classList.add('condition');
+    
+    const first = document.createElement('span');
+    first.classList.add('forecast-data');
+    first.textContent = currentWeather.forecast.condition;
+
+    const second = document.createElement('span');
+    second.classList.add('forecast-data');
+    second.textContent = currentWeather.forecast.high;
+
+    const third = document.createElement('span');
+    third.classList.add('forecast-data');
+    third.textContent = currentWeather.forecast.low;
+
+    forcastDivElement.appendChild(first);
+    forcastDivElement.appendChild(second);
+    forcastDivElement.appendChild(third);
 
 }
 
