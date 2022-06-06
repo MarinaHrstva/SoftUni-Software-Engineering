ffabee946a175d182c635710d2b8f7cea3abb3fc
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-06 13:27:51 +0300

    task 03-Add getLocation function

--- JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
+++ JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
@@ -1,5 +1,14 @@
 function attachEvents() {
-    console.log("TODO...");
+    
 }
 
 attachEvents();
+
+async function getLocation(location) {
+    const url = 'http://localhost:3030/jsonstore/forecaster/locations'
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data.filter(l=>l.name==location);
+
+}
