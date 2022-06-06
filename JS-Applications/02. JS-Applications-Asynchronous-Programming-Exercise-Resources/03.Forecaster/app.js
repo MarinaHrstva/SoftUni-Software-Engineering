ed5819880bbd1a12d1ef138b6d1ac2ba499f23eb
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-06 13:52:54 +0300

    Task 03-Add updomingCondition function, add btn eventListener

--- JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
+++ JS-Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
@@ -1,26 +1,31 @@
 function attachEvents() {
     const submitBtn = document.getElementById('submit');
-    const searchedLocation = document.getElementById('location').value;
-
-
-    submitBtn.addEventListener('submit', () => {
-        const currentLocation = await getLocation(searchedLocation);
-
-    })
+    submitBtn.addEventListener('click', displayWeather);
 
 }
 
 attachEvents();
+
+
+
+async function displayWeather() {
+    let searchedLocation = document.getElementById('location').value;
+    searchedLocation = await getLocation(searchedLocation);
+    const currentWeather = await getCurrentCondition(searchedLocation.code);
+const upcomingWeather
+
+    console.log(currentWeather);
+
+}
 
 async function getLocation(location) {
     const url = 'http://localhost:3030/jsonstore/forecaster/locations'
     const res = await fetch(url);
     const data = await res.json();
 
-    return data.filter(l => l.name == location);
+    return data.filter(l => l.name == location)[0];
 
 }
-
 
 async function getCurrentCondition(code) {
     const url = 'http://localhost:3030/jsonstore/forecaster/today/' + code;
@@ -28,4 +33,13 @@ async function getCurrentCondition(code) {
     const data = await res.json();
 
     return data;
+}
+
+async function getUpcommingCondition(code) {
+    const url = 'http://localhost:3030/jsonstore/forecaster/upcoming/' + code;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+
 }
