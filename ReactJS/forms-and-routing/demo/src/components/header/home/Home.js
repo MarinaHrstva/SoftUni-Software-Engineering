fae5bc4c45533cacf4a8d8a2c8ed2aa212bac22f
Marina Hristova <marina.ts.hristova@gmail.com>
2022-07-30 16:54:41 +0300

    List latest games from server

--- ReactJS/forms-and-routing/demo/src/components/header/home/Home.js
+++ ReactJS/forms-and-routing/demo/src/components/header/home/Home.js
@@ -1,19 +1,23 @@
 import { useState, useEffect } from 'react'
+
+import LatestGame from './latest-game/LatestGame'
+
+
 import * as gameService from '../../../services/gameServise'
 
 
 function Home() {
 
-    const [games, setGames] = useState()
+    const [games, setGames] = useState([])
 
     useEffect(() => {
         gameService.getAll()
             .then(result => {
-                console.log(result)
+                setGames(result);
             })
     }, [])
 
-    
+
     return (
         <section id="welcome-world">
             <div className="welcome-message">
@@ -23,65 +27,13 @@ function Home() {
             <img src="./images/four_slider_img01.png" alt="hero" />
             <div id="home-page">
                 <h1>Latest Games</h1>
-
-                {/* <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/CoverFire.png" />
-                    </div>
-                    <h3>Cover Fire</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/ZombieLang.png" />
-                    </div>
-                    <h3>Zombie Lang</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/MineCraft.png" />
-                    </div>
-                    <h3>MineCraft</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div> */}
-
-                <p className="no-articles">No games yet</p>
+                {
+                    games.length > 0
+                        ? games.map(game => <LatestGame game={game} />)
+                        : < p className="no-articles">No games yet</p>
+                }
             </div>
-        </section>
+        </section >
     )
 }
 
