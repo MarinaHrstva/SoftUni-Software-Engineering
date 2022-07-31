32098e829f550d0fb0a8cb0b61aa9db922852496
Marina Hristova <marina.ts.hristova@gmail.com>
2022-07-31 11:27:18 +0300

    Add login and register components

--- ReactJS/forms-and-routing/demo/src/App.js
+++ ReactJS/forms-and-routing/demo/src/App.js
@@ -1,9 +1,9 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Header from './components/header/Header'
 import Home from './components/home/Home';
-
-import Login from './components/home/login/login';
+import Login from './components/login/login';
+import Register from './components/reggister/Register';
 
 
 
@@ -11,16 +11,17 @@ function App() {
   return (
 
     <div id="box">
-      <Header/>
+      <Header />
       <main id="main-content">
-     <Routes>
-<Route path='/' element={<Home/>}/>
-<Route path='/login' element={<Login/>}/>
-
-
-     </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />}/>
+          <Route path='/'
+        </Routes>
+          
       </main>
-   
+
     </div>
 
   );
