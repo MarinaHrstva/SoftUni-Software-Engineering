f15a765f03d77f3f935e52e27faf35c71a970fb6
Marina Hristova <marina.ts.hristova@gmail.com>
2022-08-01 11:16:58 +0300

    Add change handlers to username, password, age and bio

--- ReactJS/forms-demo/src/App.js
+++ ReactJS/forms-demo/src/App.js
@@ -5,12 +5,18 @@ import './App.css';
 function App() {
 
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [age, setAge] = useState('');
+  const [bio, setBio] = useState('');
+  const [tac, setTac] = useState('');
+  const [userType, setUserType] = useState('corporate');
+  const [gender, setGender] = useState('f');
+
 
 
   const submitHandler = (e) => {
     e.preventDefault();
-
-
+console.log(password, age, username, bio);
   }
 
   const onUsernameChange = (e) => {
@@ -28,25 +34,30 @@ function App() {
           </div>
 
           <div>
-            <label htmlFor="password">Password:</label>
+            <label htmlFor="password" value={password} onChange={(e) => setPassword(e.target.value)}>Password:</label>
             <input type="password" id='password' />
           </div>
 
           <div>
+            <label htmlFor="age">Age:</label>
+            <input type="number" name='age' id='age' value={age} onChange={e=>setAge(e.target.value)} />
+          </div>
+
+          <div>
             <label htmlFor="bio">Bio</label>
-            <textarea name="bio" id="bio" cols="30" rows="10" />
+            <textarea name="bio" id="bio" cols="30" rows="10" value={bio} onChange={e=>setBio(e.target.value)}/>
           </div>
 
           <div>
             <label htmlFor="tac">Terms and Conditions:</label>
-            <input type="checkbox"  name='tac' id='tac'/>
+            <input type="checkbox" name='tac' id='tac' />
           </div>
 
           <div>
             <label htmlFor="individual-user-type">Individual:</label>
-            <input type="radio" name='user-type' value='individual' id='individual-user-type'/>
+            <input type="radio" name='user-type' value='individual' id='individual-user-type' />
             <label htmlFor="corporate-user-type">Corporate:</label>
-            <input type="radio" name='user-type' value='corporate' id='corporate-user-type'/>
+            <input type="radio" name='user-type' value='corporate' id='corporate-user-type' />
           </div>
 
           <div>
