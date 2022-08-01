eac2268da1806cda7962336cc6ed7d4cc93502d9
Marina Hristova <marina.ts.hristova@gmail.com>
2022-08-01 12:36:27 +0300

    Update App.js

--- ReactJS/forms-demo/src/App.js
+++ ReactJS/forms-demo/src/App.js
@@ -3,25 +3,28 @@ import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [values, setValues] = useState({
+    username:'',
+    password:'',
+    age:'',
+    bio:'',
+    gender:'f',
+    userType:'corporate',
+    tac:true
+  });
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [age, setAge] = useState('');
-  const [bio, setBio] = useState('');
-  const [tac, setTac] = useState(false);
-  const [userType, setUserType] = useState('corporate');
-  const [gender, setGender] = useState('f');
-
-
+  const changeHandler = (e) => {
+    setValues(state => ({
+      ...state,
+      [e.target.name]: e.target.type == 'checkbox' ? e.target.checked : e.target.value
+    }))
+  }
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(password, age, username, bio, gender, userType, tac);
+    console.log(values);
   }
 
-  const onUsernameChange = (e) => {
-    setUsername(e.target.value)
-  }
 
   return (
     <div className="App">
@@ -30,39 +33,39 @@ function App() {
 
           <div>
             <label htmlFor="username">Username:</label>
-            <input type="text" id='username' value={username} onChange={onUsernameChange} />
+            <input name='username' type="text" id='username' value={values.username} onChange={changeHandler} />
           </div>
 
           <div>
-            <label htmlFor="password" value={password} onChange={(e) => setPassword(e.target.value)}>Password:</label>
-            <input type="password" id='password' />
+            <label htmlFor="password" value={values.password} onChange={changeHandler}>Password:</label>
+            <input type="password" id='password' name='password' />
           </div>
 
           <div>
             <label htmlFor="age">Age:</label>
-            <input type="number" name='age' id='age' value={age} onChange={e => setAge(e.target.value)} />
+            <input type="number" name='age' id='age' value={values.age} onChange={changeHandler} />
           </div>
 
           <div>
             <label htmlFor="bio">Bio</label>
-            <textarea name="bio" id="bio" cols="30" rows="10" value={bio} onChange={e => setBio(e.target.value)} />
+            <textarea name="bio" id="bio" cols="30" rows="10" value={values.bio} onChange={changeHandler} />
           </div>
 
           <div>
             <label htmlFor="tac">Terms and Conditions:</label>
-            <input type="checkbox" name='tac' id='tac' checked={tac} onChange={e => setTac(e.target.value === 'on')} />
+            <input type="checkbox" name='tac' id='tac' checked={values.tac} onChange={changeHandler} />
           </div>
 
           <div>
             <label htmlFor="individual-user-type">Individual:</label>
-            <input type="radio" name='user-type' value='individual' id='individual-user-type' onChange={e => setUserType(e.target.value)} checked={userType == 'individual'} />
+            <input type="radio" name='user-type' value='individual' id='individual-user-type' onChange={changeHandler} checked={values.userType == 'individual'} />
             <label htmlFor="corporate-user-type">Corporate:</label>
-            <input type="radio" name='user-type' value='corporate' id='corporate-user-type' onChange={e => setUserType(e.target.value)} checked={userType == 'corporate'} />
+            <input type="radio" name='user-type' value='corporate' id='corporate-user-type' onChange={changeHandler} checked={values.userType == 'corporate'} />
           </div>
 
           <div>
             <label htmlFor="gender" ></label>
-            <select name="gender" id="gender" value={gender} onChange={e => setGender(e.target.value)}>
+            <select name="gender" id="gender" value={values.gender} onChange={changeHandler}>
               <option value="m">Male</option>
               <option value="f">Female</option>
             </select>
